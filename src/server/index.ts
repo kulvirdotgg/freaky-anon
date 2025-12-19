ce741f80ee31196b47eb3421b5def44d7b6a66fe
Kulvir Singh <kulvirs734@gmail.com>
2025-12-19 18:02:37 +0530

    feat: create anon room endpoint

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,10 +1,15 @@
 import { Elysia } from "elysia"
+import { roomRouter } from "./room"
 
-const app = new Elysia({ prefix: "/api" }).get("/health", () => {
-	return {
-		success: true,
-		message: "uWu onii chan!!",
-	}
-})
+const app = new Elysia({ prefix: "/api" })
+	.get("/health", () => {
+		return {
+			success: true,
+			message: "uWu onii chan!!",
+		}
+	})
+	.use(roomRouter)
 
 export { app }
+
+export type App = typeof app
