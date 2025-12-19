2e10db70a76c47405099f82fead70ce214bcd68d
Kulvir Singh <kulvirs734@gmail.com>
2025-12-19 19:22:24 +0530

    feat: clicking join button joins to the chat room

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,22 @@
 "use client"
 
+import { useMutation } from "@tanstack/react-query"
 import { Dice4 } from "lucide-react"
+import { useRouter } from "next/navigation"
 import { useEffect, useState } from "react"
 
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Label } from "@/components/ui/label"
+import { client } from "@/lib/api-client"
 import { generateRandomName } from "@/lib/generate-random-name"
 
 const NAME_KEY = "anon-user-name"
 
 export default function Page() {
 	const [name, setName] = useState("")
+
+	const router = useRouter()
 
 	useEffect(() => {
 		function main() {
@@ -29,6 +34,18 @@ export default function Page() {
 		main()
 	}, [])
 
+	const { mutate: joinRoom } = useMutation({
+		mutationKey: ["join-chat-room"],
+		mutationFn: async () => {
+			const res = await client.room.post({ ttl: 180 })
+
+			const roomId = res.data?.roomId
+			if (res.status === 200) {
+				router.push(`/room/${roomId}`)
+			}
+		},
+	})
+
 	return (
 		<main className="min-h-screen flex flex-col items-center justify-center p-4">
 			<Card className="p-6 w-full max-w-md backdrop-blur-xl">
@@ -42,7 +59,9 @@ export default function Page() {
 					</div>
 				</CardContent>
 				<CardFooter className="w-full">
-					<Button className="w-full text-sm font-semibold">join freaky room</Button>
+					<Button className="w-full text-sm font-semibold" onClick={() => joinRoom()} type="button">
+						join anon chat room
+					</Button>
 				</CardFooter>
 			</Card>
 		</main>
