3688e4edd79100d8f2869d12692c336fc2467ae6
Kulvir Singh <kulvirs734@gmail.com>
2025-12-22 14:17:57 +0530

    feat: centeralized error handler

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,11 +3,37 @@ import { Elysia } from "elysia"
 import { roomRouter } from "@/server/routes/room"
 
 const app = new Elysia({ prefix: "/api" })
-	.get("/health", () => {
-		return {
-			success: true,
-			message: "uWu onii chan!!",
+	.onError(({ code, error, status }) => {
+		if (process.env.NODE_ENV !== "production") {
+			console.error(error)
 		}
+
+		switch (code) {
+			case "VALIDATION": {
+				return error.detail(error.message)
+			}
+			case "NOT_FOUND": {
+				return {
+					status: 404,
+					message: "Route not found",
+				}
+			}
+			case "UNKNOWN":
+			case "INTERNAL_SERVER_ERROR": {
+				let errMsg = "An internal server error has occurred"
+				if (error.message) {
+					errMsg = error.message
+				}
+
+				return status(500, {
+					status: 500,
+					message: errMsg,
+				})
+			}
+		}
+	})
+	.get("/health", ({ status }) => {
+		return status(200, { message: "uWu oniiiChan!!!!!" })
 	})
 	.use(roomRouter)
 
