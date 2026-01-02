33cd418c37e37422d6ea2987faa144e6802be4c7
Kulvir Singh <kulvirs734@gmail.com>
2026-01-02 15:37:45 +0530

    feat: validate user before joining room

---
+++ src/proxy.ts
@@ -0,0 +1,54 @@
+import { nanoid } from "nanoid"
+import { type NextRequest, NextResponse } from "next/server"
+
+import { env } from "@/server/utils/env"
+import { redis } from "@/server/utils/redis"
+
+export const proxy = async (req: NextRequest) => {
+	const pathname = req.nextUrl.pathname
+
+	const roomMatch = pathname.match(/^\/room\/([^/]+)$/)
+	if (!roomMatch) {
+		return NextResponse.redirect(new URL("/", req.url))
+	}
+
+	// roomId user is tyring to connect to
+	const roomId = roomMatch[1]
+	const redisRoomKey = `room:${roomId}`
+
+	const room = await redis.hgetall<{ connected: string[]; createdAt: number }>(redisRoomKey)
+	if (!room) {
+		return NextResponse.redirect(new URL("/?error=invalid-room", req.url))
+	}
+
+	// if user refresh the page etc, let user join the room again
+	const existingAuthToken = req.cookies.get("x-auth-token")?.value
+	if (existingAuthToken && room.connected.includes(existingAuthToken)) {
+		return NextResponse.next()
+	}
+
+	// do not allow more than 2 users in a single room
+	if (room.connected.length >= 2) {
+		return NextResponse.redirect(new URL("/?error=room-full", req.url))
+	}
+
+	const response = NextResponse.next()
+
+	const authToken = nanoid()
+	response.cookies.set("x-auth-token", authToken, {
+		path: "/",
+		secure: env.NODE_ENV === "production",
+		sameSite: "strict",
+		httpOnly: true,
+	})
+
+	await redis.hset(redisRoomKey, {
+		connected: [...room.connected, authToken],
+	})
+
+	return response
+}
+
+export const config = {
+	matcher: "/room/:path*",
+}
