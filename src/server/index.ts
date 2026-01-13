c159acf1044f867a24504ac67ac7aff10d458833
Kulvir Singh <kulvirs734@gmail.com>
2026-01-14 00:09:45 +0530

    chore: not needed request logger

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,9 @@
 import { Elysia } from "elysia"
 
 import { errorHandler } from "@/server/middleware/error-handler"
-import { requestLogger } from "@/server/middleware/request-logger"
 import { roomRouter } from "@/server/routes/room"
 
 const app = new Elysia({ prefix: "/api" })
-	.use(requestLogger)
 	.use(errorHandler)
 	.get("/health", async ({ status }) => {
 		return status(200, { message: "uWu oniiiChan!!!!!" })
