2abb4e50fcdce943b21e5d3df1960f3e53a50666
Kulvir Singh <kulvirs734@gmail.com>
2026-01-20 01:44:03 +0530

    chore: cleaned up proxy file

--- src/proxy.ts
+++ src/proxy.ts
@@ -3,27 +3,30 @@ import { type NextRequest, NextResponse, URLPattern } from "next/server"
 import { redis, redisKey } from "@/lib/redis"
 
 export const proxy = async (req: NextRequest) => {
+	const url = new URL("/", req.url)
+
 	const pattern = new URLPattern({ pathname: "/room/:roomId" })
-	const match = pattern.exec(req.nextUrl.href)
+	const match = pattern.exec(url)
 	if (!match) {
-		return NextResponse.redirect(new URL("/", req.url))
+		return NextResponse.redirect(new URL("/", url))
 	}
 
 	const roomId = match.pathname.groups.roomId as string
-
 	const roomKey = redisKey("room", roomId)
 
 	const connected = await redis.hget<string[]>(roomKey, "connected")
 	const authToken = req.cookies.get("x-auth-token")?.value as string
 
-	// if room does not exist.. lmaooo
+	// if room does not exist KEKW
 	if (!connected) {
-		return NextResponse.redirect(new URL("/?error=invalid-room-id", req.url))
+		url.searchParams.set("error", "invalid-room-id")
+		return NextResponse.redirect(url)
 	}
 
 	// if user does not belongs to the room
 	if (!connected.includes(authToken)) {
-		return NextResponse.redirect(new URL("/?error=unauthorized", req.url))
+		url.searchParams.set("error", "unauthorized")
+		return NextResponse.redirect(url)
 	}
 
 	const response = NextResponse.next()
