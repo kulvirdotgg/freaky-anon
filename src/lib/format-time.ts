ee7e2c8a0d74b7ca1b91bae32957433fbdc34e03
Kulvir Singh <kulvirs734@gmail.com>
2026-02-07 16:47:46 +0530

    feat: format timestamp method

--- src/lib/format-time.ts
+++ src/lib/format-time.ts
@@ -1,5 +1,14 @@
-export function formatTime(seconds: number) {
+export function formatSecondsToMinutes(seconds: number) {
 	const mins = Math.floor(seconds / 60)
 	const secs = seconds % 60
 	return `${mins}:${secs.toString().padStart(2, "0")}`
+}
+
+export function formatTimestamp(timestamp: number) {
+	const date = new Date(timestamp)
+	return date.toLocaleTimeString("en-US", {
+		hour: "2-digit",
+		minute: "2-digit",
+		hour12: true,
+	})
 }
