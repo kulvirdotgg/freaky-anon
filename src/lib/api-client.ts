f7104c2378d33d777da80530e7c0846a87c02f67
Kulvir Singh <kulvirs734@gmail.com>
2026-03-05 02:14:43 +0530

    feat: correct use of API_URL in the treaty client

--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -2,4 +2,4 @@ import { treaty } from "@elysiajs/eden"
 
 import type { App } from "@/server"
 
-export const client = treaty<App>("http://localhost:3000").api
+export const client = treaty<App>(process.env.NEXT_PUBLIC_API_URL!).api
